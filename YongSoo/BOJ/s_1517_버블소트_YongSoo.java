8e8616173d7c46d7bffb9efe02f16132aa4e1512
조용수 <soo_0201@naver.com>
2024-06-04 22:41:17 +0900

    [BOJ] 버블소트 / YongSoo / 2236ms

--- YongSoo/BOJ/s_1517_버블소트_YongSoo.java
+++ YongSoo/BOJ/s_1517_버블소트_YongSoo.java
@@ -8,7 +8,8 @@ import java.util.StringTokenizer;
 
 /**
  * inversion counting
- * 세그먼트 트리로 해결
+ * 펜윅 트리로 해결
+ * (세그먼트 트리와 펜윅 트리 두 가지 방법이 있으나, 펜윅 트리가 조금 더 간편한 듯)
  *
  * 버블 소트에서, (자신보다 큰 숫자와 swap되는 횟수) = (자신 왼쪽에 있는 자신보다 큰 숫자의 개수)이다.
  * 각 수열의 원소에 대해 자신보다 큰 숫자와 swap되는 횟수를 더해 주면 전체 swap 횟수를 얻을 수 있으므로,
@@ -19,7 +20,7 @@ import java.util.StringTokenizer;
  * 2. 배열 정렬
  * 3. 큰 수부터, inversion counting 시작
  *    3-1. 자신 왼쪽에 있는 자신보다 큰 수의 개수를 구함
- *    3-2. segmentTree 업데이트
+ *    3-2. fenwickTree 업데이트
  * 4. 정답 출력
  */
 public class s_1517_버블소트_YongSoo {
@@ -27,7 +28,7 @@ public class s_1517_버블소트_YongSoo {
     static StringTokenizer st;
     static int N;
     static Node[] arr;
-    static int[] segmentTree;
+    static int[] fenwickTree;
     static long answer;
     static class Node {
         int value;
@@ -60,50 +61,43 @@ public class s_1517_버블소트_YongSoo {
             return Integer.compare(n1.value, n2.value);
         });
 
-        // 세그먼트 트리 초기화
-        int n = (int) Math.ceil(Math.log(N) / Math.log(2));
-
-        int treeSize = (1 << (n+1));
-        segmentTree = new int[treeSize];
+        // 펜윅 트리 초기화
+        fenwickTree = new int[N+1];
 
 
         // 3. 큰 수부터, inversion counting 시작
         for (int sortedIdx=N-1; sortedIdx>=0; sortedIdx--) {
-
             Node node = arr[sortedIdx];
             int idx = node.idx;
 
             // 3-1. 자신 왼쪽에 있는 자신보다 큰 수의 개수를 구함
-            int biggerNum = getBiggerNum(1, 0, idx-1, 0, N-1);
+            int biggerNum = getBiggerNum(idx);
             answer += biggerNum;
-            // 3-2. segmentTree 업데이트
-            updateBiggerNum(1, idx, 0, N-1);
+            // 3-2. fenwickTree 업데이트
+            updateBiggerNum(idx);
         }
 
         System.out.println(answer);
     }
 
-    private static void updateBiggerNum(int nodeIdx, int idx, int start, int end) {
-        if (start > idx || end < idx) return;
+    private static void updateBiggerNum(int idx) {
+        int cnt = 0;
+        while (idx < fenwickTree.length) {
+            fenwickTree[idx]++;
+            idx += ((idx) & (-idx));
 
-        segmentTree[nodeIdx]++;
-
-        if (start < end) {
-            updateBiggerNum(2*nodeIdx, idx, start, (start+end)/2);
-            updateBiggerNum(2*nodeIdx+1, idx, (start+end)/2+1, end);
+            if (++cnt == 20) break;
         }
     }
 
-    // from ~ to 사이의 구간합을 구하는 메서드
-    // inversion counting에 사용
-    private static int getBiggerNum(int nodeIdx, int from, int to, int start, int end) {
-        if (to < start || from > end) return 0;
-        if (from <= start && end <= to) return segmentTree[nodeIdx];
-
-        return getBiggerNum(2*nodeIdx, from, to, start, (start+end)/2) +
-                getBiggerNum(2*nodeIdx+1, from, to, (start+end)/2+1, end);
+    private static int getBiggerNum(int idx) {
+        int biggerNum = 0;
+        while (idx > 0) {
+            biggerNum += fenwickTree[idx];
+            idx -= ((idx) & (-idx));
+        }
+        return biggerNum;
     }
-
 
     private static void setInitVariable() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -113,7 +107,7 @@ public class s_1517_버블소트_YongSoo {
         st = new StringTokenizer(br.readLine().trim());
         for (int idx=0; idx<N; idx++) {
             int value = Integer.parseInt(st.nextToken());
-            arr[idx] = new Node(value, idx);
+            arr[idx] = new Node(value, idx+1);
         }
 
         answer = 0;
