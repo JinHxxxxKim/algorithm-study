a47840d8e66c44a1b4cae04800cbee2a251f2778
DESKTOP-MMCQPV1\SSAFY <gjrudghks1@naver.com>
2024-08-05 08:11:18 +0900

    [SWEA] 승강제리그 / GyeongHwan / n / 시간초과

--- GyeongHwan/SWEA/s_승강제리그(UserSolution)_GyeongHwan.java
+++ GyeongHwan/SWEA/s_승강제리그(UserSolution)_GyeongHwan.java
@@ -1,76 +1,57 @@
-package Btest;
 import java.util.*;
 /**
- * 1. 함수호출 횟수 확인
- * 	1-1. move()함수는 500회 호출
- * 	1-2. trade()함수는 1000회 호출
- * : trade를 좀 더 빠르게 구현하면 되겠다.
+ * 1. 제약조건 및 함수호출 확인
+ *	1-1. trade() 호출 <= 1000
+ *	1-2. move()	호출 <= 500
+ *	1-3. 선수들의 수 <= 39990
+ *	1-4. 리그의 개수 <= 10
  *
- * 2. 제약사항 확인
- * : 선수들의 수가 왜 39990이라는 숫자에 제한이 걸려있지?
- * : 리그의 수가 왜 10개로 제한되어 있지?
- *
- * 리그의 수==10개인 경우
- * move(), trade()할때마다 이동하는 선수들은 총 18명
- * 이동 후, 각 리그 정렬
  */
 
 /**
- * 첫번째 문제풀이 아이디어 - 단순 구현
+ * 1. 리그별로 선수들을 어떤 자료구조로 저장해야 하는가?
+ * 	1-1. 배열 => 접근은 빠르지만 선수이동 후 매번 정렬해야함
+ * 	1-2. 자동 정렬이 되는 자료구조는 뭐가 있을까?
+ * 		1-2-1. 우선순위 큐 => 중간값을 처리할 수 없음
+ * 		1-2-2. 트리셋 => 처음, 끝의 검색이 자유로움. 중간은 iterator로 접근가능
+ *  1-3. 각 리그마다 트리셋으로 생성
  *
- * 1. 선수 클래스 생성
- * 	1-1. id, ability
- * 	1-2. 정렬조건: ability 내림차순, id 오름차순
+ * 2. 각 리그에서 모두 한꺼번에 선수들을 제거한 다음, 한꺼번에 넣기
+ * 	2-1. 0번째 리그의 좋은 선수와, 마지막 리그의 나쁜 선수는 이동 없음
  *
- * 2. 리그 배열
- * 	2-1. league = arr[l][n/l]
- * 	2-2. 중간선수: arr[][(n/l+1)/2-1]
- * 	2-3. 좋은선수: arr[][0]
- * 	2-4. 나쁜선수: arr[][n/l-1]
- *
- * 3. move 또는 trade 후 각 리그 정렬
- *
- * => 제한시간 초과 발생 
- * : 배열접근해서 값바꾸는 건 시간소모가 크지않다. 
- * : 함수호출마다 매번 리그별로 정렬해주는 것이 가장 시간소모가 크다.
- * -> 리그를 배열이 아닌 자동으로 정렬해주는 자료구조를 사용하자.
- * : 자동으로 정렬해주는 자료구조 
- * -> 우선순위 큐: 가장 큰 값이나 작은 값은 뺄 수 있지만 중간값은 못뺌
  */
 
 /**
- * 두번째 문제풀이 아이디어 - PQ
- * 
- * 1. 선수 클래스 생성
- * 2. 각 리그를 pq로 생성
- * 3. move()
- * 	3-1. tempBottomAbility[idx]에 idx번째 리그에서 가장 부족한 선수를 빼서 넣기
- *  3-2. tempTopAbility[idx]에 idx번째 리그에서 가장 뛰어난 선수를 빼서 넣기
- *  	3-2-1. reversedPQ생성 후 idx번째 리그의 선수 다 넣기
- *  	3-2-2. reversedPQ에서 가장 뛰어난 선수 빼기
- *  	3-2-3. 새로 PQ[idx] 생성 후, 가장 뛰어난 선수가 빠진 후 남아있는 선수들 다 넣기
- * 4. trade()
- * 	4-1. Arrays.sort()보다 Collections.sort()가 더 빠르므로 ArraysList에 넣고 처리
- * 
- * => 제한시간 초과 발생
- * : 계속 자료구조를 새로 생성하고 데이터를 넣는 과정이 많아서 그런 듯....
- * 
+ * move()
+ *
+ * 1. 0번째 리그의 좋은 선수와 마지막 리그의 나쁜 선수를 빼서 따로 저장
+ * 2. 1번째 리그(now)의 좋은 선수와 0번째 리그(prev)의 나쁜 선수 교환
+ * 3. 반복
+ * 4. 0번째 리그의 좋은 선수와 마지막 리그의 나쁜 선수를 추가
+ *
  */
 
 /**
- * 세번째 문제풀이 아이디어 - TreeSet
+ * trade()
+ *
+ * 1. 0번째 리그의 좋은 선수와 마지막 리그의 중간 선수를 빼서 따로 저장
+ * 2. 1번째 리그(now)의 좋은 선수와 0번째 리그(prev)의 중간 선수 교환
+ * 3. 반복
+ * 4. 0번째 리그의 좋은 선수와 마지막 리그의 중간 선수를 추가
+ *
  */
 
 
 
 class UserSolution {
-	static class Player implements Comparable<Player>{
-		int id, ability;
+	public class Player implements Comparable<Player>{
+		int id;
+		int ability;
 		public Player(int id, int ability){
 			this.id=id;
 			this.ability=ability;
 		}
-		@Override
+
 		public int compareTo(Player o){
 			if(this.ability==o.ability){
 				return this.id-o.id;
@@ -78,11 +59,12 @@ class UserSolution {
 			return o.ability-this.ability;
 		}
 	}
-	
-	static int N, L;
-	static int[] ability;
-	static TreeSet<Player>[] leagues;
-	static int playerPerLeague;
+	int N;
+	int L;
+	int[] ability;
+	int playerPerLeague;
+	TreeSet<Player>[] leagues;
+
 
 	void init(int N, int L, int mAbility[]) {
 		this.N=N;
@@ -93,64 +75,76 @@ class UserSolution {
 		for(int idx=0; idx<L; idx++) {
 			leagues[idx] = new TreeSet<>();
 		}
-		for(int id=0; id<N; id++) {
-			int leagueNum=id/playerPerLeague;
-			leagues[leagueNum].add(new Player(id,mAbility[id]));
+		for(int idx=0; idx<N; idx++){
+			int leagueNum = idx/playerPerLeague;
+			leagues[leagueNum].add(new Player(idx, ability[idx]));
 		}
 	}
 
 	int move() {
 		int ans=0;
-		Player[] topPlayers = new Player[L];
-		Player[] bottomPlayers = new Player[L];
-		
-		for(int idx=0; idx<L; idx++) {
-			//각 리그에서 능력이 뛰어난 선수 따로 저장
-			topPlayers[idx]=leagues[idx].first();
-			//각 리그에서 능력이 부족한 선수 따로 저장
-			bottomPlayers[idx]=leagues[idx].last();
+		//각 리그에서 좋은 선수와 나쁜 선수들 따로 저장해둘 리스트
+		Player[] tempGoodPlayer = new Player[L];
+		Player[] tempBadPlayer = new Player[L];
+
+		//각 리그에서 좋은 선수와 나쁜 선수들 빼기
+		for(int now=0; now<L; now++){
+			tempGoodPlayer[now] = leagues[now].first();
+			tempBadPlayer[now] = leagues[now].last();
+
+			leagues[now].remove(tempGoodPlayer[now]);
+			leagues[now].remove(tempBadPlayer[now]);
 		}
-		
-		//선수 교환하기
-		for(int idx=1; idx<L; idx++) {
-			leagues[idx-1].remove(bottomPlayers[idx-1]);
-			leagues[idx].remove(topPlayers[idx]);
-			
-			leagues[idx-1].add(topPlayers[idx]);
-			leagues[idx].add(bottomPlayers[idx-1]);
-			
-			ans+=topPlayers[idx].id+bottomPlayers[idx-1].id;
+
+		//이동한 결과를 각 리그에 넣기
+		for(int now=1; now<L; now++){
+			int prev=now-1;
+			leagues[now].add(tempBadPlayer[prev]);
+			leagues[prev].add(tempGoodPlayer[now]);
+
+			ans+=tempBadPlayer[prev].id+tempGoodPlayer[now].id;
 		}
+
+		//0번째 리그의 좋은 선수와 마지막 리그의 나쁜 선수 넣기
+		leagues[0].add(tempGoodPlayer[0]);
+		leagues[L-1].add(tempBadPlayer[L-1]);
 
 		return ans;
 	}
 
 	int trade() {
 		int ans=0;
-		int mid=playerPerLeague/2;
-		Player[] topPlayers = new Player[L];
-		Player[] midPlayers = new Player[L];
-		
-		for(int idx=0; idx<L; idx++) {
-			//각 리그에서 뛰어난 선수 따로 저장
-			topPlayers[idx]=leagues[idx].first();
-			//각 리그에서 중간 선수 따로 저장
-			Iterator<Player> iterator = leagues[idx].iterator();
-			for(int idx2=0; idx2<mid; idx2++) {
+		int midIndex=playerPerLeague/2;
+		//각 리그에서 좋은 선수와 중간 선수들 따로 저장해둘 리스트
+		Player[] tempGoodPlayer = new Player[L];
+		Player[] tempMidPlayer = new Player[L];
+
+		//각 리그에서 좋은 선수와 중간 선수들 빼기
+		for(int now=0; now<L; now++){
+			tempGoodPlayer[now] = leagues[now].first();
+
+			Iterator<Player> iterator = leagues[now].iterator();
+			for(int idx=0; idx<midIndex; idx++){
 				iterator.next();
 			}
-			midPlayers[idx]=iterator.next(); //현재 가리키고 있는 요소를 반환
+			tempMidPlayer[now] = iterator.next();
+
+			leagues[now].remove(tempGoodPlayer[now]);
+			leagues[now].remove(tempMidPlayer[now]);
 		}
-		
-		for(int idx=1; idx<L; idx++) {
-			leagues[idx-1].remove(midPlayers[idx-1]);
-			leagues[idx].remove(topPlayers[idx]);
-			
-			leagues[idx-1].add(topPlayers[idx]);
-			leagues[idx].add(midPlayers[idx-1]);
-			
-			ans+=midPlayers[idx-1].id+topPlayers[idx].id;
+
+		//이동한 결과를 각 리그에 넣기
+		for(int now=1; now<L; now++){
+			int prev=now-1;
+			leagues[now].add(tempMidPlayer[prev]);
+			leagues[prev].add(tempGoodPlayer[now]);
+
+			ans+=tempMidPlayer[prev].id+tempGoodPlayer[now].id;
 		}
+
+		//0번째 리그의 좋은 선수와 마지막 리그의 중간 선수 넣기
+		leagues[0].add(tempGoodPlayer[0]);
+		leagues[L-1].add(tempMidPlayer[L-1]);
 
 		return ans;
 	}
